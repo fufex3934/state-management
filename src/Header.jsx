2d6c667ec4c2123a35b0d99de7ec0acbe72d3842
fufex3934 <fufawakgari174@gmail.com>
2025-05-02 08:33:04 +0300

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+import chefClaudeLogo from "/chef-claude-icon.png"
+export default function Header() {
+  return (
+    <header>
+      <img src={chefClaudeLogo} alt="chef-icon" />
+      <h1>Chef Claude</h1>
+    </header>
+  )
+}
